de2bcc2437cb2808ec84232309520e977b62b38a
CHEREL Antoine <cherel.antoine@outlook.com>
2022-01-27 10:40:52 +0100

    new robot model & faq texts

--- src/react-components/home/faqTexts.js
+++ src/react-components/home/faqTexts.js
@@ -1,54 +1,54 @@
 export default [
   {
-    title: "Kann ich CyberCinity auch mit einer niedrigeren Internet-Brandbreite nutzen?",
+    title: "Can I use CyberCinity with a lower internet bandwidth?",
     items: [
       {
-        title: 'Ja, Sie können CyberCinity auch mit einer niedrigeren Internet-Bandbreite nutzen. \n Auch eine Internetverbindung langsamer als 1 MB/s ist möglich. Die Szenen können bis zu 128 MB betragen und werden während des Ladens der Webseite gleichzeitig geladen.',
+        title: 'Yes, you can use CyberCinity with a lower internet bandwidth. \ n An Internet connection slower than 1 MB / s is also possible. The scenes can be up to 128 MB and are loaded simultaneously while the website is loading.',
         href: `/internet-connection`
       },
     ]
   },
   {
-    title: "Müssen spezielle Internetadressen oder Ports zugelassen werden?",
+    title: "Do special internet addresses or ports have to be allowed?",
     items: [
       {
-        title: "Ja, Sie sollten die folgenden HTTPS und WSS (Web Sockets) freischalten: \n *.cybercinity.net (port 443 & 80) \n cci.imsimity.com (port 443) \n*.cybercinity.workers.dev (port 443) \nfonts.googleapis.com (port 433)",
+        title: "Yes, you should enable the following HTTPS and WSS (Web Sockets): \ n * .cybercinity.net (port 443) \ n cci.imsimity.com (port 443) \ n * .cybercinity.workers.dev (port 443) \ nfonts.googleapis.com (port 433)",
         href: `/internet-connection`
       },
     ]
   },
   {
-    title: "Wie kann ich meinen Avatar auswählen?",
+    title: "How can I choose my avatar?",
     items: [
       {
-        title: "Sie können Ihren Avatar im Menü namens \"More\" unten auf der rechten Seite unter \"Change Name & Avatar\" aufrufen. Im daraufhin erscheinenden Editor können Sie das Aussehen Ihres Avatars ändern. Sie haben die Möglichkeit, einen Standard-Avatar zu wählen oder einen eigenen Avatar von Grund auf selbst zu gestalten.",
+        title: "You can find your avatar in the menu called \"More\" at the bottom right under \"Change Name & Avatar\". In the editor that appears you can change the appearance of your avatar. You have the option of choosing a standard avatar or designing your own from scratch.",
         href: `/choose-avatar`
       }
     ]
   },
   {
-    title: "Wo lege ich meinen Nutzernamen fest?",
+    title: "Where do I set my username?",
     items: [
       {
-        title: "Sie können Ihren Nutzernamen im Menü namens \"More\"auf der unteren rechten Seite unter \"Change Name & Avatar\" aufrufen. Im daraufhin erscheinenden Eingabefeld namens \"Display Name\" können Sie ihren Nutzernamen festlegen.",
+        title: "You can call up your username in the menu called \"More\" on the lower right under \"Change Name & Avatar\". In the input field called \" Display Name \" you can specify your username.",
         href: `/change-username`
       }
     ]
   },
   {
-    title: "Wie navigiere ich?",
+    title: "How do i navigate?",
     items: [
       {
-        title: "Die Navigation am Desktop erfolgt intuitiv über die WASD- oder Pfeiltasten. Die rechte Maustaste kann genutzt werden, um sich an eine andere Stelle im Raum zu teleportieren. Darüber hinaus können Sie im Raum schneller navigieren, wenn Sie gleichzeitig die Shift-Taste gedrückt halten. Mit den Tasten Q und E können Sie den Blickwinkel ändern. Alternativ können Sie aber auch die linke Maustaste gedrückt halten und gleichzeitig über den Bildschirm ziehen.",
+        title: "Navigation on the desktop is intuitive using the WASD or arrow keys. The right mouse button can be used to teleport to another point in the room. In addition, you can navigate the room faster if you hold down the Shift key at the same time. Use the Q and E keys to change the angle of view. Alternatively, you can also hold down the left mouse button and drag it across the screen at the same time.",
         href: `/navigation`
       }
     ]
   },
   {
-    title: "Wie groß ist die Kapazität des Raums?",
+    title: "What is the capacity of the room?",
     items: [
       {
-        title: "Die Räume haben eine maximale Kapazität von 25 Personen, um die Leistung aller Geräte zu gewährleisten. In der Lobby können jedoch viel mehr Personen beitreten. Die Benutzer in der Lobby können sehen und hören, was im Raum vor sich geht. Außerdem können sie über den Chat mit anderen interagieren.",
+        title: "The rooms have a maximum capacity of 25 people to ensure the performance of all equipment. However, many more people can join in the lobby. The users in the lobby can see and hear what is going on in the room. They can also interact with others through chat.",
         href: `/room-capacity`
       }
     ]
