ed80b81965f6d19c8e6dcf2c23965aabd57e5681
Antoine Cherel <ac@imsimity.de>
2022-03-25 11:02:08 +0100

    notebook modal

---
+++ src/react-components/room/NotebookModalContainer.js
@@ -0,0 +1,32 @@
+import React, { useCallback } from "react";
+import PropTypes from "prop-types";
+import { NotebookModal } from "./NotebookModal";
+import configs from "../../utils/configs";
+import ducky from "../../assets/models/DuckyMesh.glb";
+
+const isMobile = AFRAME.utils.device.isMobile() || AFRAME.utils.device.isMobileVR();
+
+export function NotebookModalContainer({ scene, onClose }) {
+  const onSubmit = useCallback(
+    ({ file, url }) => {
+      scene.emit("add_media", (file && file.length > 0 && file[0]) || url || ducky);
+      onClose();
+    },
+    [scene, onClose]
+  );
+
+  return (
+    <NotebookModal
+      isMobile={isMobile}
+      showModelCollectionLink={configs.feature("show_model_collection_link")}
+      modelCollectionUrl={configs.link("model_collection", "https://sketchfab.com/mozillareality")}
+      onSubmit={onSubmit}
+      onClose={onClose}
+    />
+  );
+}
+
+NotebookModalContainer.propTypes = {
+  scene: PropTypes.object.isRequired,
+  onClose: PropTypes.func
+};
