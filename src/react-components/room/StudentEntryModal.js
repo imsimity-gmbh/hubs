86556b48746d8a1d266e904780185c941209f32e
antoinech <cherel.antoine@outlook.com>
2022-09-08 09:07:27 +0200

    Security notice

--- src/react-components/room/StudentEntryModal.js
+++ src/react-components/room/StudentEntryModal.js
@@ -97,6 +97,8 @@ export function StudentEntryModal ({ scene, showNonHistoriedDialog, onClose, sec
         {
             showAcceptBtn = false;
             showSecurityBtn = true;
+
+            onClose();
         }
         else
         {
@@ -244,10 +246,13 @@ export function StudentEntryModal ({ scene, showNonHistoriedDialog, onClose, sec
                 /*beforeTitle={<CloseButton onClick={onClose} />}*/
             >
                 <div className="scrollableContent">
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   
-                    Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.   
-                    Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.   
-                    Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer
+                    In unserem virtuellen Lehr-Lern-Labor gibt es keine Gefahrenquellen für dich.
+                    Für das Arbeiten im Labor gibt es trotzdem ein paar Dinge, die du beachten solltest.
+                    · Während des Aufenthalts im Labor musst du immer einen Kittel und eine Schutzbrille
+                    tragen, auch wenn du selbst nicht experimentierst!
+                    · Denk daran sauber und sorgfältig zu arbeiten, weil es sonst zu Fehlern beim Experiment
+                    kommen kann!
+                    · Essen und Trinken sind im Labor verboten!
                 </div>
                 <Button className={acceptBtnClassName} onClick={() => backToStudentEntry(showNonHistoriedDialog, scene)}>
                     <FormattedMessage id="security-reading.close" defaultMessage="Bestätigen"/>
