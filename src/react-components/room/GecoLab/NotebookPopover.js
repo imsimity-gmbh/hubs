dd567c3927d7b2576a69a6228a4e3a20a6cd0d65
ArneThiele <at@imsimity.de>
2022-09-29 16:02:08 +0200

    fixed and reworked notebook

--- src/react-components/room/GecoLab/NotebookPopover.js
+++ src/react-components/room/GecoLab/NotebookPopover.js
@@ -1,24 +1,56 @@
-import React from "react";
+import React, {} from "react";
 import PropTypes from "prop-types";
-import { ButtonGridPopover } from "../../popover/ButtonGridPopover";
 import { Popover } from "../../popover/Popover";
 import { ToolbarButton } from "../../input/ToolbarButton";
 import { ReactComponent as NotebookIcon } from "../../icons/GecoLab/Notebook.svg";
 import { defineMessage, useIntl } from "react-intl";
+import { TextAreaInputField } from "../../input/TextAreaInputField";
+import Cookies from "js-cookie";
+import { Button } from "../../input/Button";
+import { useForm } from "react-hook-form";
+import { Column } from "../../layout/Column";
 
 const notebookPopoverTitle = defineMessage({
   id: "notebook-popover.title",
   defaultMessage: "Notebook"
 });
 
-export function NotebookPopoverButton({ items }) {
+export function NotebookPopoverButton({ items, onClose }) {
   const intl = useIntl();
   const title = intl.formatMessage(notebookPopoverTitle);
+  const {} = useForm();
+
+  const handleOnChange = event => {
+    if((typeof event.target.value === 'string' || event.target.value instanceof String))
+      {
+      let jsonNotes = JSON.stringify(event.target.value);
+      Cookies.set('notes', jsonNotes);
+      //console.log("Speicher " + event.target.value + " in " + jsonNotes);
+      }
+  }
+  
+  let styles = {
+    backgroundColor: 'black',
+    height: '200px',
+    width: '200px',
+  };
 
   return (
     <Popover
       title={title}
-      content={props => <ButtonGridPopover items={items} {...props} />}
+      content={props =>
+          <Column as="form" center>
+            <TextAreaInputField 
+              style = {styles} 
+              defaultValue = {JSON.parse(Cookies.get("notes"))}
+              onChange={handleOnChange} />
+            <Button
+              preset="accept"
+              onClick={onClose}>
+                Save
+            </Button>
+          </Column>
+        }
       placement="top"
       offsetDistance={28}
       disableFullscreen
@@ -38,5 +70,6 @@ export function NotebookPopoverButton({ items }) {
 }
 
 NotebookPopoverButton.propTypes = {
-  items: PropTypes.array.isRequired
+  items: PropTypes.array.isRequired,
+  onClose: PropTypes.func
 };
--- src/react-components/room/GecoLab/NotebookPopoverContainer.js
+++ src/react-components/room/GecoLab/NotebookPopoverContainer.js
@@ -1,45 +1,22 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { NotebookPopoverButton } from "./NotebookPopover";
-import { ReactComponent as ReadIcon } from "../../icons/GecoLab/Read.svg";
-import { ReactComponent as WriteIcon } from "../../icons/GecoLab/Write.svg";
-import { FormattedMessage } from "react-intl";
-import { NotebookModalContainer } from "./NotebookModalContainer";
 
 
 export function NotebookPopoverContainer({scene, showNonHistoriedDialog}) {
 
   const [items, setItems] = useState([]);
 
-  function showReadModal() {
-    showNonHistoriedDialog(
-      NotebookModalContainer, 
-      { scene, writeBtn: false,
-        onNoteDeleted: showReadModal}
-    )
-  }
-
   useEffect(
     () => {
       function updateItems() {
-
+        
         let nextItems = [
           {
-            id: "read",
-            icon: ReadIcon,
-            color: "accent5",
-            label: <FormattedMessage id="notebook-popover.read" defaultMessage="Lesen" />,
-            onSelect: () => showReadModal()
-          },
-          {
-            id: "write",
-            icon: WriteIcon,
-            color: "accent5",
-            label: <FormattedMessage id="notebook-popover.write" defaultMessage="Schreiben" />,
-            onSelect: () => showNonHistoriedDialog(NotebookModalContainer, { scene, writeBtn: true})
+            id: "text",
           }
         ];
-
+        
         setItems(nextItems);
       }
 
@@ -58,3 +35,10 @@ NotebookPopoverContainer.propTypes = {
   scene: PropTypes.object.isRequired,
   showNonHistoriedDialog: PropTypes.func.isRequired
 };
+/*
+id: "write",
+icon: WriteIcon,
+color: "accent5",
+label: <FormattedMessage id="notebook-popover.write" defaultMessage="Schreiben" />,
+onSelect: () => showNonHistoriedDialog(NotebookModalContainer, { scene, writeBtn: true})
+*/
