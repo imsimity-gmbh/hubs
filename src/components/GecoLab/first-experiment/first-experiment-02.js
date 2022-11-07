c986b5a1be5d4f415658d3f3ba61cac5c057be01
antoinech <cherel.antoine@outlook.com>
2022-11-07 14:23:56 +0100

    fix issues with interactables

--- src/components/GecoLab/first-experiment/first-experiment-02.js
+++ src/components/GecoLab/first-experiment/first-experiment-02.js
@@ -328,6 +328,14 @@ const tongModelPromise = waitForDOMContentLoaded().then(() => loadModel(tongSrc)
             s.components["entity-socket"].subscribe("onSnap", this.onPlacedExperimentItem);
         });
         
+        if (this.isMember)
+        {
+            this.movableEntities.forEach(e => {
+                let name = e.className;
+                e.className = "interactable " + name;
+            });
+        }
+        
         
         // Mannequin
         this.mannequin = this.el.sceneEl.systems["mannequin-manager"].getMannequinByGroupCode(this.experimentData.groupCode);
