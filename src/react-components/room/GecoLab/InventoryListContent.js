0670e1f1e2051bfee0018a1f85575e0950a36ce7
ArneThiele <at@imsimity.de>
2022-11-07 13:49:22 +0100

    optimize Inventory Pics

--- src/react-components/room/GecoLab/InventoryListContent.js
+++ src/react-components/room/GecoLab/InventoryListContent.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { Column } from "../../layout/Column";
 import styles from "./InventoryListContent.scss";
@@ -18,44 +18,46 @@ import inv_tripod_with_triangle from "../../../assets/models/GecoLab/preview/inv
 import { auto } from "@popperjs/core";
 
 export function InventoryListContent() {
-  
+  const [bold, setBold] = useState(1);
+
   return (
     <Column className={styles.inventoryContent} padding>
       <img id="inventory-preview-picture" style={{width: "200px", marginLeft:auto, marginRight:auto}} src={inv_bunsen_burner}></img>
         <div id="inventory-content" style={{width: "100%"}}>
             <ul>
-              <li onMouseEnter={event=>changeImagePreview(1)} onClick={event=>changeImagePreview(1)}>Bunsenbrenner</li>
-              <li onMouseEnter={event=>changeImagePreview(2)} onClick={event=>changeImagePreview(2)}>Mörser und Stößel</li>
-              <li onMouseEnter={event=>changeImagePreview(3)} onClick={event=>changeImagePreview(3)}>Dreifuß mit aufgelegter Ceran-Schutzplatte</li>
-              <li onMouseEnter={event=>changeImagePreview(4)} onClick={event=>changeImagePreview(4)}>Dreifuß mit aufgelegtem Tondreieck</li>
-              <li onMouseEnter={event=>changeImagePreview(5)} onClick={event=>changeImagePreview(5)}>Feuerzeug</li>
-              <li onMouseEnter={event=>changeImagePreview(6)} onClick={event=>changeImagePreview(6)}>Thermometer</li>
-              <li onMouseEnter={event=>changeImagePreview(7)} onClick={event=>changeImagePreview(7)}>Glasstab zum Rühren</li>
-              <li onMouseEnter={event=>changeImagePreview(8)} onClick={event=>changeImagePreview(8)}>Ausgewählte Bodenprobe</li>
-              <li onMouseEnter={event=>changeImagePreview(9)} onClick={event=>changeImagePreview(9)}>Tiegelzange</li>
-              <li onMouseEnter={event=>changeImagePreview(10)} onClick={event=>changeImagePreview(10)}>Löffel für Probe</li>
-              <li onMouseEnter={event=>changeImagePreview(11)} onClick={event=>changeImagePreview(11)}>Waage (erscheint im Laufe des Experiments)</li>
-              <li onMouseEnter={event=>changeImagePreview(12)} onClick={event=>changeImagePreview(12)}>Tiegel (erscheint im Laufe des Experiments)</li>
+              <li onMouseEnter={event=>changeImagePreview(1)} onClick={event=>changeImagePreview(1)} style={{fontWeight: bold==1 ? 'bold' : 'normal'}}>Bunsenbrenner</li>
+              <li onMouseEnter={event=>changeImagePreview(2)} onClick={event=>changeImagePreview(2)} style={{fontWeight: bold==2 ? 'bold' : 'normal'}}>Mörser und Stößel</li>
+              <li onMouseEnter={event=>changeImagePreview(3)} onClick={event=>changeImagePreview(3)} style={{fontWeight: bold==3 ? 'bold' : 'normal'}}>Dreifuß mit aufgelegter Ceran-Schutzplatte</li>
+              <li onMouseEnter={event=>changeImagePreview(4)} onClick={event=>changeImagePreview(4)} style={{fontWeight: bold==4 ? 'bold' : 'normal'}}>Dreifuß mit aufgelegtem Tondreieck</li>
+              <li onMouseEnter={event=>changeImagePreview(5)} onClick={event=>changeImagePreview(5)} style={{fontWeight: bold==5 ? 'bold' : 'normal'}}>Feuerzeug</li>
+              <li onMouseEnter={event=>changeImagePreview(6)} onClick={event=>changeImagePreview(6)} style={{fontWeight: bold==6 ? 'bold' : 'normal'}}>Thermometer</li>
+              <li onMouseEnter={event=>changeImagePreview(7)} onClick={event=>changeImagePreview(7)} style={{fontWeight: bold==7 ? 'bold' : 'normal'}}>Glasstab zum Rühren</li>
+              <li onMouseEnter={event=>changeImagePreview(8)} onClick={event=>changeImagePreview(8)} style={{fontWeight: bold==8 ? 'bold' : 'normal'}}>Ausgewählte Bodenprobe</li>
+              <li onMouseEnter={event=>changeImagePreview(9)} onClick={event=>changeImagePreview(9)} style={{fontWeight: bold==9 ? 'bold' : 'normal'}}>Tiegelzange</li>
+              <li onMouseEnter={event=>changeImagePreview(10)} onClick={event=>changeImagePreview(10)} style={{fontWeight: bold==10 ? 'bold' : 'normal'}}>Löffel für Probe</li>
+              <li onMouseEnter={event=>changeImagePreview(11)} onClick={event=>changeImagePreview(11)} style={{fontWeight: bold==11 ? 'bold' : 'normal'}}>Waage (erscheint im Laufe des Experiments)</li>
+              <li onMouseEnter={event=>changeImagePreview(12)} onClick={event=>changeImagePreview(12)} style={{fontWeight: bold==12 ? 'bold' : 'normal'}}>Tiegel (erscheint im Laufe des Experiments)</li>
             </ul>
         </div>
     </Column>
   );
-}
 
-function changeImagePreview(nummer){
-  switch(nummer){
-    case 1:document.getElementById("inventory-preview-picture").src = inv_bunsen_burner; break;
-    case 2:document.getElementById("inventory-preview-picture").src = inv_mortar_with_stick; break;
-    case 3:document.getElementById("inventory-preview-picture").src = inv_tripod_with_plate; break;
-    case 4:document.getElementById("inventory-preview-picture").src = inv_tripod_with_triangle; break;
-    case 5:document.getElementById("inventory-preview-picture").src = inv_lighter; break;
-    case 6:document.getElementById("inventory-preview-picture").src = inv_thermo; break;
-    case 7:document.getElementById("inventory-preview-picture").src = inv_glassstick; break;
-    case 8:document.getElementById("inventory-preview-picture").src = inv_groundSample; break;
-    case 9:document.getElementById("inventory-preview-picture").src = inv_tong; break;
-    case 10:document.getElementById("inventory-preview-picture").src = inv_spoon; break;
-    case 11:document.getElementById("inventory-preview-picture").src = inv_scale; break;
-    case 12:document.getElementById("inventory-preview-picture").src = inv_crucible; break;
+  function changeImagePreview(nummer){
+    setBold(nummer);
+    switch(nummer){
+      case 1:document.getElementById("inventory-preview-picture").src = inv_bunsen_burner; break;
+      case 2:document.getElementById("inventory-preview-picture").src = inv_mortar_with_stick; break;
+      case 3:document.getElementById("inventory-preview-picture").src = inv_tripod_with_plate; break;
+      case 4:document.getElementById("inventory-preview-picture").src = inv_tripod_with_triangle; break;
+      case 5:document.getElementById("inventory-preview-picture").src = inv_lighter; break;
+      case 6:document.getElementById("inventory-preview-picture").src = inv_thermo; break;
+      case 7:document.getElementById("inventory-preview-picture").src = inv_glassstick; break;
+      case 8:document.getElementById("inventory-preview-picture").src = inv_groundSample; break;
+      case 9:document.getElementById("inventory-preview-picture").src = inv_tong; break;
+      case 10:document.getElementById("inventory-preview-picture").src = inv_spoon; break;
+      case 11:document.getElementById("inventory-preview-picture").src = inv_scale; break;
+      case 12:document.getElementById("inventory-preview-picture").src = inv_crucible; break;
+    }
   }
 }
 
