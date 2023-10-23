3b384dda999cf081fc012194428bbca5dd976f5a
ArneThiele <at@imsimity.de>
2023-10-23 19:22:25 +0200

    First Steps, now Debugging Models

--- src/components/GecoLab/third-experiment/third-experiment-01.js
+++ src/components/GecoLab/third-experiment/third-experiment-01.js
@@ -1,8 +1,8 @@
 import { waitForDOMContentLoaded } from "../../../utils/async-utils";
 import { cloneObject3D } from "../../../utils/three-utils";
 import { loadModel } from "../.././gltf-model-plus";
-import growthCabinetSrc from "../../../assets/models/GecoLab/PlantGrowth/geco_growth_cabinet.glb";
-import plantSrc from "../../../assets/models/GecoLab/PlantGrowth/wheat_co2_1.glb";
+import growthCabinetSrc from "../../../assets/models/GecoLab/PlantGrowth/geco_saucer.glb";
+import plantSrc from "../../../assets/models/GecoLab/scales_container.glb";
 import { IMSIMITY_INIT_DELAY } from "../../../utils/imsimity";
 import { decodeNetworkId, getNetworkIdFromEl } from "../../../utils/GecoLab/network-helper";
 
@@ -64,7 +64,7 @@ const plantPromise =  waitForDOMContentLoaded().then(() => loadModel(plantSrc));
               // Fallback, we trigger manualy the delayed init
               this.delayedInit();
             }*/
-           
+            this.delayedInit();
           });  
         }, IMSIMITY_INIT_DELAY * 0.9);
 
@@ -84,18 +84,18 @@ const plantPromise =  waitForDOMContentLoaded().then(() => loadModel(plantSrc));
       this.plantStartWrapper = this.el.querySelector(".plant-Start-Wrapper");
      
       this.growthCabinet1 = this.el.querySelector(".growth-Cabinet-1");
-      this.spawnGrowthCabinet(growthCabinetPromise, new THREE.Vector3(0, 0, 0), this.growthCabinet1, false);
+      this.spawnItem(growthCabinetPromise, new THREE.Vector3(1.3, 0.8, 0.2), this.growthCabinet1, false);
       this.growthCabinet2 = this.el.querySelector(".growth-Cabinet-2");
-      this.spawnGrowthCabinet(growthCabinetPromise, new THREE.Vector3(0, 0, 0.15), this.growthCabinet2, false);
+      this.spawnItem(growthCabinetPromise, new THREE.Vector3(2, 0.8, 0.2), this.growthCabinet2, false);
       this.growthCabinet3 = this.el.querySelector(".growth-Cabinet-3");
-      this.spawnGrowthCabinet(growthCabinetPromise, new THREE.Vector3(0, 0, 0.3), this.growthCabinet3, false);
+      this.spawnItem(growthCabinetPromise, new THREE.Vector3(2.7, 0.8, 0.2), this.growthCabinet3, false);
 
-      this.plantPlace1 = this.el.querySelector("plant-Place-1");
-      this.spawnGrowthCabinet(plantPromise, new THREE.Vector3(-1, 0, 0), this.plantPlace1, false);
-      this.plantPlace2 = this.el.querySelector("plant-Place-2");
-      this.spawnGrowthCabinet(plantPromise, new THREE.Vector3(-1, 0, 0.15), this.plantPlace2, false);
-      this.plantPlace3 = this.el.querySelector("plant-Place-3");
-      this.spawnGrowthCabinet(plantPromise, new THREE.Vector3(-1, 0, 0.3), this.plantPlace3, false);
+      this.plantPlace1 = this.el.querySelector(".plant-Place-1");
+      this.spawnItem(plantPromise, new THREE.Vector3(0, 3, 0), this.plantPlace1, false);
+      this.plantPlace2 = this.el.querySelector(".plant-Place-2");
+      this.spawnItem(plantPromise, new THREE.Vector3(2, 3, 0), this.plantPlace2, false);
+      this.plantPlace3 = this.el.querySelector(".plant-Place-3");
+      this.spawnItem(plantPromise, new THREE.Vector3(4, 3, 0), this.plantPlace3, false);
 
       this.updateUI();
 
@@ -130,7 +130,7 @@ const plantPromise =  waitForDOMContentLoaded().then(() => loadModel(plantSrc));
     },
     
     updateUI: function() {
-      if(this.localExpClicked != this.data.sexpClicked) {
+      if(this.localExpClicked != this.data.expClicked) {
         this.showPlants();
         this.localExpClicked = this.data.expClicked;
       }
@@ -140,7 +140,7 @@ const plantPromise =  waitForDOMContentLoaded().then(() => loadModel(plantSrc));
 
     },
 
-    spawnGrowthCabinet(promise, position, entity, show) {
+    spawnItem(promise, position, entity, show) {
       promise.then(model => {
           entity.object3D.visible = false;
           const mesh = cloneObject3D(model.scene);
@@ -155,7 +155,7 @@ const plantPromise =  waitForDOMContentLoaded().then(() => loadModel(plantSrc));
           entity.object3D.rotation.set(0, 0, 0);
           entity.setAttribute("position", {x: position.x, y: position.y, z: position.z});
           entity.object3D.matrixNeedsUpdate = true;
-          console.log(entity);
+
       });
     },
 
@@ -164,8 +164,12 @@ const plantPromise =  waitForDOMContentLoaded().then(() => loadModel(plantSrc));
       console.log('Show Ground Sample');
 
       this.growthCabinet1.object3D.visible = true;
-      this.growthCabinet1.object3D.visible = true;
-      this.growthCabinet1.object3D.visible = true;
+      this.growthCabinet2.object3D.visible = true;
+      this.growthCabinet3.object3D.visible = true;
+
+      this.plantPlace1.object3D.visible = true;
+      this.plantPlace2.object3D.visible = true;
+      this.plantPlace3.object3D.visible = true;
 
       if (this.isMember)
       {
@@ -179,7 +183,7 @@ const plantPromise =  waitForDOMContentLoaded().then(() => loadModel(plantSrc));
     
         NAF.utils.takeOwnership(networkedEl);
   
-        this.el.setAttribute("third-experiment", "expClicked", true);      
+        this.el.setAttribute("third-experiment-01", "expClicked", true);      
   
         this.updateUI();
       });
@@ -187,6 +191,8 @@ const plantPromise =  waitForDOMContentLoaded().then(() => loadModel(plantSrc));
 
     showPlants()
     {
+      this.thirdExpStartBtn.object3D.visible = false;
+      
       this.plantPlace1.object3D.visible = true;
       this.plantPlace2.object3D.visible = true;
       this.plantPlace3.object3D.visible = true;
